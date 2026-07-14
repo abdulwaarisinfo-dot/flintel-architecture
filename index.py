1a02856abd29441c41a8b1bccefc0d6b4f37c5ee
WARIS ALI <abdulwaarisinfo@gmail.com>
2026-07-15 01:58:13 +0500

    Update index.py

--- index.py
+++ index.py
@@ -17,6 +17,6 @@ async def internal_page():
     return FileResponse("templates/web.html")
 
 
-@app.get("/web.html")
+@app.get("/__internal__/page")
 async def block_web():
     raise HTTPException(status_code=404, detail="Not Found")
